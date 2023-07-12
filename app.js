453e81065fb8fc68cf9fbe3f046c6bf5e516d47f
Glich29 <sbislava@gmail.com>
2023-07-12 21:59:20 +0300

    Add mongo db yelp-camp schema and base tests for it

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+// Connect MongoDB at default port 27017.
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error in DB connection: "));
+db.once("open", () => {
+    console.log('MongoDB Connection Succeeded.');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, resp) => {
     resp.render('home');
+});
+
+app.get('/campground/add', async (req, resp) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    resp.send(camp);
 });
 
 app.listen(3000, () => {
